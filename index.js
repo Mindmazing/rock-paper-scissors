533288f9919eeb3850e4a5807f70e5347fc25983
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-05-28 14:31:31 -0600

    Create game logic

--- index.js
+++ index.js
@@ -18,4 +18,58 @@ function getHumanChoice() {
   return choice.toLowerCase();
 }
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+function incrementPoint(winner) {
+  return winner + 1;
+}
+
+function showGame(computerChoice) {
+  console.log(`Computer Played: ${computerChoice}`);
+}
+
+function playRound(humanChoice, computerChoice) {
+  showGame(computerChoice);
+  if (computerChoice === humanChoice) {
+    console.log("TIE");
+  } else if (humanChoice === "scissors") {
+    switch (computerChoice) {
+      case "rock":
+        console.log("Rock beats Scissors. YOU LOSE!");
+        computerScore = incrementPoint(computerScore);
+        break;
+      case "paper":
+        console.log("Scissors beats Rock. YOU WIN!");
+        humanScore = incrementPoint(humanScore);
+        break;
+    }
+  } else if (humanChoice === "rock") {
+    switch (computerChoice) {
+      case "scissors":
+        console.log("Rock beats Scissors. YOU WIN!");
+        humanScore = incrementPoint(computerScore);
+        break;
+      case "paper":
+        console.log("Paper beats Rock. YOU LOSE!");
+        computerScore = incrementPoint(humanScore);
+        break;
+    }
+  } else {
+    switch (computerChoice) {
+      case "rock":
+        console.log("Paper beats rock. YOU WIN!");
+        humanScore = incrementPoint(computerScore);
+        break;
+      case "scissors":
+        console.log("Scissors beats paper. YOU LOSE!");
+        computerScore = incrementPoint(humanScore);
+        break;
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
